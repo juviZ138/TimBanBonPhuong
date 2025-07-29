740cc6eac0d2de6a9014bfd49ec0cf46fd6ebe37
juviZ138 <quachlydientam@gmail.com>
2025-07-29 18:25:59 +0700

    Implement like feature

---
+++ client/src/core/services/likes-service.ts
@@ -0,0 +1,40 @@
+import { inject, Injectable, signal } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient, HttpParams, HttpRequest } from '@angular/common/http';
+import { LikesParams, Member } from '../../types/member';
+import { PaginatedResult } from '../../types/pagination';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LikesService {
+  private baseUrl = environment.apiUrl;
+  private http = inject(HttpClient);
+  likeIds = signal<string[]>([]);
+
+  toggleLike(targetMemberId: string) {
+    return this.http.post(`${this.baseUrl}likes/${targetMemberId}`, {});
+  }
+
+  getLikes(likesParams: LikesParams) {
+    let params = new HttpParams();
+
+    params = params.append('pageNumber', likesParams.pageNumber);
+    params = params.append('pageSize', likesParams.pageSize);
+    params = params.append('predicate', likesParams.predicate);
+
+    return this.http.get<PaginatedResult<Member>>(this.baseUrl + 'likes', {
+      params,
+    });
+  }
+
+  getLikeIds() {
+    return this.http.get<string[]>(this.baseUrl + 'likes/list').subscribe({
+      next: (ids) => this.likeIds.set(ids),
+    });
+  }
+
+  clearLikeIds() {
+    this.likeIds.set([]);
+  }
+}
--- client/src/features/lists/lists.ts
+++ client/src/features/lists/lists.ts
@@ -1,11 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { LikesService } from '../../core/services/likes-service';
+import { LikesParams, Member } from '../../types/member';
+import { MemberCard } from '../members/member-card/member-card';
+import { PaginatedResult } from '../../types/pagination';
+import { Paginator } from '../../shared/paginator/paginator';
 
 @Component({
   selector: 'app-lists',
-  imports: [],
+  imports: [MemberCard, Paginator],
   templateUrl: './lists.html',
-  styleUrl: './lists.css'
+  styleUrl: './lists.css',
 })
-export class Lists {
+export class Lists implements OnInit {
+  private likeService = inject(LikesService);
+  // protected member = signal<Member[]>([]);
+  protected paginatedMembers = signal<PaginatedResult<Member> | null>(null);
+  protected predicate = 'liked';
+  protected likesParams = new LikesParams();
 
+  tabs = [
+    { label: 'Liked', value: 'liked' },
+    { label: 'Liked me', value: 'likedBy' },
+    { label: 'Mutual', value: 'mutual' },
+  ];
+
+  ngOnInit(): void {
+    this.loadLikes();
+  }
+
+  setPredicate(predicate: string) {
+    if (this.likesParams.predicate !== predicate) {
+      this.likesParams.predicate = predicate;
+      this.likesParams.pageNumber = 1;
+      this.loadLikes();
+    }
+  }
+
+  loadLikes() {
+    this.likeService.getLikes(this.likesParams).subscribe({
+      next: (member) => {
+        this.paginatedMembers.set(member);
+      },
+    });
+  }
+
+  onPageChange(event: { pageNumber: number; pageSize: number }) {
+    this.likesParams.pageSize = event.pageSize;
+    this.likesParams.pageNumber = event.pageNumber;
+    this.loadLikes();
+  }
 }
